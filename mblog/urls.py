7f4a12d4e895438d6df32353a0903cc8f88b7827
Matías Varela <matu.varela@gmail.com>
2014-03-19 19:13:37 -0300

    Projecto django, requirements

---
+++ mblog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mblog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include('zinnia.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
